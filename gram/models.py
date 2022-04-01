8af8160b436bb6ea3fae7ebdf916ec1fb464ca53
Lewis <lewis.murgor@student.moringaschool.com>
2022-04-01 12:05:31 +0300

    create models

--- gram/models.py
+++ gram/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+    profile_photo = models.ImageField(upload_to = 'images/')
+    Bio = models.TextField()
+
+class Image(models.Model):
+    image = models.ImageField(upload_to = 'images/')
+    name = models.CharField(max_length =60)
+    caption = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    profile = models.ForeignKey(Profile,on_delete=models.CASCADE)
+    likes = models.IntegerField()
+    comments = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
