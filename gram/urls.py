785248ce48df15c3494f7015da4c438c8adc8485
Lewis <lewis.murgor@student.moringaschool.com>
2022-04-03 01:11:26 +0300

    add urls

--- gram/urls.py
+++ gram/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls.static import static
 urlpatterns=[
     re_path('^$',views.index,name='index'),
     re_path(r'^timeline/$', views.timeline, name='timeline'),
+    re_path(r'^search/', views.search_results, name='search_results'),
     re_path(r'^image/(\d+)',views.image,name ='image'),
     re_path(r'^new/image$', views.new_image, name='new-image'),
     re_path(r'^accounts/profile', views.profile, name='profile')
