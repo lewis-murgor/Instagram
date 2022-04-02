07cf475c7dca92cc2fb3beba79bcabc7ddd8093e
Lewis <lewis.murgor@student.moringaschool.com>
2022-04-02 15:30:23 +0300

    create more urls

--- gram/urls.py
+++ gram/urls.py
@@ -5,6 +5,10 @@ from django.conf.urls.static import static
 
 urlpatterns=[
     re_path('^$',views.index,name='index'),
+    re_path(r'^timeline/$', views.timeline, name='timeline'),
+    re_path(r'^image/(\d+)',views.image,name ='image'),
+    re_path(r'^new/image$', views.new_image, name='new-image'),
+    re_path(r'^accounts/profile', views.profile, name='profile')
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
