d805bbeaebf49c49f878b9d1e4ecc4d1aa1035e0
Lewis <lewis.murgor@student.moringaschool.com>
2022-04-04 01:21:26 +0300

    create forms

--- gram/forms.py
+++ gram/forms.py
@@ -1,7 +1,12 @@
 from django import forms
-from .models import Image
+from .models import Image,Profile
 
 class NewImageForm(forms.ModelForm):
     class Meta:
         model = Image
         exclude = ['date','profile','likes','comments']
+
+class UpdateProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ['user']
